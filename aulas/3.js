813e51bab6b3c595514711cdc39b2b147fbbb470
Ryam Soares da Silva <144704225+ryamsoares-s@users.noreply.github.com>
2025-04-30 17:31:07 +0000

    Adiciona `aula3.js`

---
+++ aulas/3.js
@@ -0,0 +1,13 @@
+let nome;
+
+const prompt = require("prompt-sync")();
+
+//nome = prompt("Qual seu nome?");
+
+// for (let i = 1; i < 10; i++) {
+//   console.log("Meu nome é " + nome);
+// }
+
+for (let i = 1; i < 10; i++) {
+  console.log(i);
+}
