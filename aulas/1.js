2f67500e9efb18c13811a4520cbd75163c1f3f7f
Ryam Soares da Silva <144704225+ryamsoares-s@users.noreply.github.com>
2025-04-30 16:49:47 +0000

    Adiciona aula1 `1.js` e aula2 `2.js`

---
+++ aulas/1.js
@@ -0,0 +1,8 @@
+let nome = "Ryam"; // Nesta linha estou criando uma variavel que recebe meu nome.
+let idade = 22; // Nesta linha estou criando uma variavel que recebe minha idade.
+
+if (idade < 18) {
+  console.log(nome + " é menor de idade!");
+} else {
+  console.log(nome + " é maior de idade!");
+}
---
+++ aulas/2.js
@@ -0,0 +1,13 @@
+let nome;
+let idade;
+
+const prompt = require("prompt-sync")(); // atribuindo o comando de entrada do node a variavel prompt
+
+nome = prompt("Qual seu nome?");
+idade = prompt("Qual sua idade?");
+
+if (idade < 18) {
+  console.log(nome + " é menor de idade!");
+} else {
+  console.log(nome + " é menor de idade!");
+}
